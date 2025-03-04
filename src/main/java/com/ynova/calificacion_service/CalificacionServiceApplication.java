8fedf6e33ae97041317477dfcdbb61aff4953070
Yeferson Nova <yeferson_nova@outlook.com>
2025-03-04 04:30:58 -0500

    Initial commit

---
+++ src/main/java/com/ynova/calificacion_service/CalificacionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ynova.calificacion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalificacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalificacionServiceApplication.class, args);
+	}
+
+}
